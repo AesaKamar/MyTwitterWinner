751201a11d5cdbde95a4168d40a89c0e10691ba6
Aesa Kamar <Aesa.Kamar@Gmail.com>
2016-05-13 10:13:55 -0400

    Initial Commit

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    consumer_key: "",
-    consumer_secret: "",
-    token: "",
-    token_secret: ""
+    consumer_key: "13uTzJS26y6MVyOgVQR5UWkhK",
+    consumer_secret: "ClQlEnztyhBmuU7f8tHEYfs8vPK3Grb4w9azZMvrAwBC8Nd7Qk",
+    token: "731112227164569600-RnORKm5biYSxCk2CO1Ek1YyYK8lnABR",
+    token_secret: "yZoeoycG5JcZYewKV5a9gOfO3owYpNh0Oo992fCUpICHu"
 
 }
